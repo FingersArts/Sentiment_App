dff3ef00194e51a0324067ef1bf2e4211eb351b0
Aziz Mujahiddin Nugraha <87057489+FingersArts@users.noreply.github.com>
2024-08-10 09:27:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,83 +2,76 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from collections import Counter
-from preprocessing import preprocess_and_calculate_tfidf, LogisticRegression, preprocess_input
+from preprocessing import preprocess_and_calculate_tfidf, LogisticRegression
 from preprocessing import plot_confusion_matrix, k_fold_cross_validation
 import matplotlib.pyplot as plt
 
 # Title of the application
-st.title('Analisis Sentimen dengan Multinomial Logistic Regression')
+st.title('Multinomial Logistic Regression Stage')
 
 # Create tabs
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Upload Data", "Preprocessing", "TF-IDF", "Multinomial Logistic Regression", "Prediksi Sentimen"])
+tab1, tab2, tab3, tab4 = st.tabs(["Upload Data", "Preprocessing", "TF-IDF Results", "Multinomial Logistic Regression"])
 
 # Tab for uploading data
 with tab1:
     # Upload CSV file
-    uploaded_file = st.file_uploader("Pilih File CSV", type=["csv"])
+    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
     # If a file is uploaded
     if uploaded_file is not None:
         # Read the CSV file
         df = pd.read_csv(uploaded_file)
 
-        # Store the original dataframe in session state
-        st.session_state['df'] = df
-
         # Display the original DataFrame
-        st.subheader('Data Asli')
+        st.subheader('Original Data')
         st.write(df)
 
 # Tab for pre-processing
 with tab2:
     if uploaded_file is not None:
         # Button to start preprocessing
-        if st.button('Mulai Preprocessing'):
+        if st.button('Start Preprocessing'):
             # Process the dataframe
-            preprocessed_df, ranking = preprocess_and_calculate_tfidf(st.session_state['df'])
+            preprocessed_df, ranking = preprocess_and_calculate_tfidf(df)
             
-            # Store preprocessed data and TF-IDF ranking in session state
-            st.session_state['preprocessed_df'] = preprocessed_df
-            st.session_state['ranking'] = ranking
-            
-            st.subheader('Data Setelah Preprocessing')
+            st.subheader('Data After Preprocessing')
             st.write(preprocessed_df[['text', 'casefolding', 'cleanedtext', 'slangremoved', 'stopwordremoved', 'stemmedtext', 'tokenize']])
 
             # Count word frequencies in the 'stemmedtext' column
             word_counts = preprocessed_df['stemmedtext'].str.split(expand=True).stack().value_counts()
             
             # Visualize the top 10 most frequent words
-            st.subheader('Visualisasi 10 Kata Teratas')
+            st.subheader('Preprocessed Data Visualization')
             st.bar_chart(word_counts.head(10))
 
             # Display the top 10 most frequent words
-            st.subheader('10 Kata Teratas')
+            st.subheader('Top 10 Words')
             st.write(word_counts.head(10))
     else:
-        st.write("Pilih File CSV di tab 'Upload Data' untuk Memulai.")
+        st.write("Please upload a CSV file in the 'Upload Data' tab to get started.")
 
 # Tab for TF-IDF results
 with tab3:
     if uploaded_file is not None:
-        if 'preprocessed_df' in st.session_state:
+        if 'preprocessed_df' in locals():
             # Display all TF-IDF ranked terms
-            st.subheader('TF-IDF')
-            st.dataframe(st.session_state['ranking'])
+            st.subheader('All Terms by TF-IDF')
+            st.dataframe(ranking)
         else:
-            st.write("Mulai tahap Preprocessing terlebih dahulu")
+            st.write("Please go to the 'Preprocessing' tab and start the Preprocessing first.")
     else:
-        st.write("Pilih File CSV di tab 'Upload Data' untuk Memulai.")
+        st.write("Please upload a CSV file in the 'Upload Data' tab to get started.")
 
 # Tab for Logistic Regression
 with tab4:
     if uploaded_file is not None:
-        if 'preprocessed_df' in st.session_state:
+        if 'preprocessed_df' in locals():
             # Map sentiment_val to label
             sentiment_mapping = {'negative': 0, 'neutral': 1, 'positive': 2}
-            st.session_state['df']['label'] = st.session_state['df']['sentiment_val'].map(sentiment_mapping)
+            df['label'] = df['sentiment_val'].map(sentiment_mapping)
 
             # Prepare data for Logistic Regression
-            words = ' '.join(st.session_state['preprocessed_df']['stemmedtext']).split()
+            words = ' '.join(preprocessed_df['stemmedtext']).split()
             word_counts = Counter(words)
             features = [word for word, count in word_counts.most_common(1000)]
 
@@ -87,20 +80,16 @@ with tab4:
                     return [words.count(feature) for feature in features]
                 return np.array([count_features(text.split()) for text in texts])
 
-            X = transform_to_features(st.session_state['preprocessed_df']['stemmedtext'], features)
-            y = st.session_state['df']['label'].values.astype(int)  # Use the newly created label column
-
-            # Store features and labels in session state
-            st.session_state['X'] = X
-            st.session_state['y'] = y
-            st.session_state['features'] = features
+            X = transform_to_features(preprocessed_df['stemmedtext'], features)
+            #X = np.array(preprocessed_df['TF_IDF_Vec'].tolist())
+            y = df['label'].values.astype(int)  # Use the newly created label column
 
             # Initialize and train the model with default parameters
             model = LogisticRegression(num_iter=200, learning_rate=0.44)
             (mean_accuracy, mean_precision, mean_recall, average_confusion, avg_class_accuracies, avg_precision_per_class, avg_recall_per_class) = k_fold_cross_validation(X, y, model, k=10)
 
             # Display model performance
-            st.subheader('Performa Model')
+            st.subheader('Model Performance')
 
             col1, col2, col3 = st.columns(3)
 
@@ -136,11 +125,11 @@ with tab4:
             st.subheader('Analisis Sentimen selama 1 Minggu Terakhir')
 
             # Konversi kolom 'date' ke format datetime dan tambahkan kolom 'tanggal'
-            st.session_state['df']['date'] = pd.to_datetime(st.session_state['df']['date'])
-            st.session_state['df']['tanggal'] = st.session_state['df']['date'].dt.strftime('%Y-%m-%d')
+            df['date'] = pd.to_datetime(df['date'])
+            df['tanggal'] = df['date'].dt.strftime('%Y-%m-%d')
 
             # Filter data untuk 1 minggu terakhir
-            df_last_week = st.session_state['df'][st.session_state['df']['date'] >= st.session_state['df']['date'].max() - pd.to_timedelta(7, unit='D')]
+            df_last_week = df[df['date'] >= df['date'].max() - pd.to_timedelta(7, unit='D')]
 
             # Hitung jumlah sentimen untuk setiap tanggal dengan label 0 = negative, 1 = netral, dan 2 = positive
             sentiment_counts = df_last_week.groupby(['tanggal', 'label']).size().unstack(fill_value=0)
@@ -160,15 +149,16 @@ with tab4:
             plt.grid(True)
             st.pyplot(plt)
 
+
         else:
-            st.write("Mulai tahap Preprocessing terlebih dahulu")
+            st.write("Please go to the 'Preprocessing' tab and start the Preprocessing first.")
     else:
-        st.write("Pilih File CSV di tab 'Upload Data' untuk Memulai.")
+        st.write("Please upload a CSV file in the 'Upload Data' tab to get started.")
 
 with tab5:
     if uploaded_file is not None:
         
-        if 'preprocessed_df' in st.session_state:
+        if 'preprocessed_df' in local:
             
             st.subheader('Prediksi Sentimen')
             # Text input from the user
